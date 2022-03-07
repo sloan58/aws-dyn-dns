1d2a11a761028512ddbe1626a85621a8f91afbcc
Marty Sloan <marty@karmatek.io>
2022-03-07 10:03:38 -0500

    add config file and logger

--- app.php
+++ app.php
@@ -2,62 +2,102 @@
 
 require './vendor/autoload.php';
 
+use Monolog\Logger;
+use Monolog\Handler\RotatingFileHandler;
+
+$logger = new Logger('app');
+$logger->pushHandler(
+    new RotatingFileHandler(__DIR__ . '/logs/app.log', 5, Logger::DEBUG)
+);
+
+$config = require __DIR__ . '/config.php';
+
 $sharedConfig = [
-    'profile' => 'default',
-    'region' => 'us-east-1',
-    'version' => 'latest'
+    'profile' => $config['aws']['profile'],
+    'region' => $config['aws']['region'],
+    'version' => $config['aws']['version']
 ];
 
 $sdk = new Aws\Sdk($sharedConfig);
 $r53 = $sdk->createRoute53();
 
-$managedZone = "somedomain.com"; // TLD
-$subdomain = "foobar"; // Subdomain (Optional)
-$fullDomain = empty($subdomain)
-    ? $managedZone
-    : sprintf('%s.%s', $subdomain, $managedZone);
+foreach($config['domains'] as $domain) {
 
-$domainARecord = getPublicIpAddressFor($fullDomain);
-$myPublicIp = myPublicIpAddress();
+    $managedZone = $domain['name'];
 
-if(($domainARecord and $myPublicIp) and ($domainARecord != $myPublicIp)) {
-    $hostedZone = getHostedZone($r53, $managedZone);
+    if($domain['validateTld']) {
+        processZone($managedZone);
+    }
 
-    if(!empty($hostedZone)) {
-        $zoneIdResponse = $hostedZone['Id'];
-        $zoneId = explode('/', $zoneIdResponse)[2] ?? null;
-        if($zoneId) {
-            $success = updateRoute53ARecord($r53, $fullDomain, $myPublicIp, $zoneId);
-            if($success) {
-                echo "Yeah!\n";
-            } else {
-                echo "Oops!\n";
+    foreach($domain['subdomains'] as $subdomain) {
+        $fullDomain = empty($subdomain)
+            ? $managedZone
+            : sprintf('%s.%s', $subdomain, $managedZone);
+        processZone($fullDomain, $managedZone);
+    }
+}
+
+function processZone($fullDomain, $managedZone = null)
+{
+    global $logger;
+
+    $managedZone = $managedZone ?: $fullDomain;
+
+    $domainARecord = getPublicIpAddressFor($fullDomain);
+    $myPublicIp = myPublicIpAddress();
+    $logger->info('@processZone: DNS lookup results', [
+        'fullDomain' => $fullDomain,
+        'domainARecord' => $domainARecord,
+        'myPublicIp' => $myPublicIp
+    ]);
+
+    if(($domainARecord and $myPublicIp) and ($domainARecord != $myPublicIp)) {
+        $hostedZone = getHostedZone($managedZone);
+
+        if(!empty($hostedZone)) {
+            $zoneIdResponse = $hostedZone['Id'];
+            $zoneId = explode('/', $zoneIdResponse)[2] ?? null;
+            if($zoneId) {
+                $success = updateRoute53ARecord($fullDomain, $myPublicIp, $zoneId);
+                if($success) {
+                    $logger->info('@processZone: Successfully updated Route53 A record');
+                } else {
+                    $logger->error('@processZone: Could not update Route53 A record');
+                }
             }
         }
     }
 }
 
-function getHostedZone($r53, $managedZone) {
+function getHostedZone($managedZone)
+{
+    global $r53;
+
     $out = $r53->listHostedZones();
     return array_values(array_filter($out['HostedZones'], function($zone) use ($managedZone) {
             return $zone['Name'] == $managedZone . '.';
         }))[0] ?? [];
 }
 
-function myPublicIpAddress() {
+function myPublicIpAddress()
+{
     $ipAddress = json_decode(
         file_get_contents('https://api.ipify.org?format=json')
     );
     return $ipAddress->ip;
 }
 
-function getPublicIpAddressFor($domain) {
+function getPublicIpAddressFor($domain)
+{
     return filter_var(
         gethostbyname($domain), FILTER_VALIDATE_IP
     ) ?: false;
 }
 
-function updateRoute53ARecord($r53, $fullDomain, $myPublicIp, $zoneId) {
+function updateRoute53ARecord($fullDomain, $myPublicIp, $zoneId): bool
+{
+    global $r53, $logger;
+
     date_default_timezone_set('America/New_York');
     $date = date('Y-m-d H:i:s');
 
@@ -85,7 +125,7 @@ function updateRoute53ARecord($r53, $fullDomain, $myPublicIp, $zoneId) {
         ]);
         return true;
     } catch (\Exception $e) {
-        var_dump($e->getMessage());
+        $logger->error('@updateRoute53ARecord: ' . $e->getMessage());
         return false;
     }
 }
